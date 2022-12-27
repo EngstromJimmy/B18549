c1c0b21169da42c3ff527f1df2351f1abcedbb15
Jimmy Engström <jimmy@apeoholic.se>
2022-12-27 16:15:56 +0100

    Added Chapter 2-5

---
+++ Chapter05/MyBlog/Data.Models/Models/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿namespace Data.Models;
+
+public class BlogPost
+{
+    public string? Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Text { get; set; } = "";
+    public DateTime PublishDate { get; set; }
+    public Category? Category { get; set; }
+    public List<Tag> Tags { get; set; } = new();
+}
