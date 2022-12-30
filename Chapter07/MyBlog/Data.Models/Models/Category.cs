6559d9c85dbd30c42ce26e94bb9458c207e916f3
Jimmy Engström <jimmy@apeoholic.se>
2022-12-30 14:00:38 +0100

    Added Chapter 6 and 7

---
+++ Chapter07/MyBlog/Data.Models/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Data.Models;
+using System.ComponentModel.DataAnnotations;
+public class Category
+{
+    public string? Id { get; set; }
+    [Required]
+    public string Name { get; set; } = "";
+}
