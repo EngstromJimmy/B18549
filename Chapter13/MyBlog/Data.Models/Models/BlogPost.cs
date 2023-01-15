4fe1866281ea8e87f598572b7688862a269d0cd2
Jimmy Engström <jimmy@apeoholic.se>
2023-01-15 22:21:56 +0100

    Added the rest of the chapters

---
+++ Chapter13/MyBlog/Data.Models/Models/BlogPost.cs
@@ -0,0 +1,14 @@
+﻿namespace Data.Models;
+using System.ComponentModel.DataAnnotations;
+public class BlogPost
+{
+    public string? Id { get; set; }
+    [Required]
+    [MinLength(5)]
+    public string Title { get; set; } = "";
+    [Required]
+    public string Text { get; set; } = "";
+    public DateTime PublishDate { get; set; }
+    public Category? Category { get; set; }
+    public List<Tag> Tags { get; set; } = new();
+}
